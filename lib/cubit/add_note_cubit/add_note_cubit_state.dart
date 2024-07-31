85a2f45a06499320b037a88c3b1e63932982e5ea
abohussein <110132240+Evil-Knight1@users.noreply.github.com>
2024-07-31 12:32:47 +0300

    add note states

--- lib/cubit/add_note_cubit/add_note_cubit_state.dart
+++ lib/cubit/add_note_cubit/add_note_cubit_state.dart
@@ -4,3 +4,13 @@ part of 'add_note_cubit_cubit.dart';
 sealed class AddNoteCubitState {}
 
 final class AddNoteCubitInitial extends AddNoteCubitState {}
+
+class AddNoteLoading extends AddNoteCubitState {}
+
+class AddNoteSuccess extends AddNoteCubitState {}
+
+class AddNoteFailure extends AddNoteCubitState {
+  final String errorMessage;
+
+  AddNoteFailure({required this.errorMessage});
+}
